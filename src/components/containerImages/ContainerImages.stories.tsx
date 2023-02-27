a41c2abebf2816bfaf62e7d003349282b2c05b48
Dayan <dayaara6@gmail.com>
2023-02-27 12:15:54 -0300

    add | añado componente que muestra al pokemon y sus evoluciones

---
+++ src/components/containerImages/ContainerImages.stories.tsx
@@ -0,0 +1,34 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { ContainerImages } from "./ContainerImages";
+import { pokemonMock } from "../../test/mocks/pokemonMock";
+import '../../index.css'
+
+export default {
+  title: "ContainerImages",
+  component: ContainerImages,
+  argTypes: {},
+} as ComponentMeta<typeof ContainerImages>;
+
+const Template: ComponentStory<typeof ContainerImages> = (args) => (
+  <ContainerImages {...args} />
+);
+
+export const Default = Template.bind({});
+
+Default.args = {
+  evolutions: pokemonMock.fire.evolutions,
+  location: pokemonMock.fire.location,
+  img: pokemonMock.fire.img,
+};
+
+export const WithoutIcons = Template.bind({});
+
+WithoutIcons.args = {
+  evolutions: [
+    { name: "charmander", id: 1, icon: undefined! },
+    { name: "charizard", id: 2, icon: undefined! },
+    { name: "charmileon", id: 3, icon: undefined! },
+  ],
+  location: pokemonMock.fire.location,
+  img: pokemonMock.fire.img,
+};
