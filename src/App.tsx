db278c8a7f7374f7e74682cdb74c8843696fefcb
Dayan Arango <65741174+dayanMichelle@users.noreply.github.com>
2023-02-26 22:55:31 -0300

    add | añado las rutas de la aplicacion (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import { Layout } from "./layout";
+import { Home } from "./pages";
+
 function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/:category/:option" element={<Home />} />
+        <Route path="*" element={<div>Not Found</div>} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { PokemonProvider } from "./context/PokemonContext";
 import "./index.css";
@@ -7,7 +8,9 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <PokemonProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </PokemonProvider>
   </React.StrictMode>
 );
