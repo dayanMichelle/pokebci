819ad8bcd7d6f3d27d39521a47591ceabdf6486f
Dayan <dayaara6@gmail.com>
2023-02-27 12:42:07 -0300

    add | añado componente donde se renderiza la lista de skills del pokemon

--- src/components/infoPokemon/InfoPokemon.stories.tsx
+++ src/components/infoPokemon/InfoPokemon.stories.tsx
@@ -1,6 +1,6 @@
-import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { InfoPokemon } from "./InfoPokemon";
+import "../../index.css";
 
 export default {
   title: "InfoPokemon",
